7d8eb764071c514c92bcf1c030d41aa3925feb84
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2014-07-15 17:54:43 +0100

    F=ma! I forgot the "m" part.
    I'm an idiot. My physics degree is real, I swear.

--- src/rk4.c
+++ src/rk4.c
@@ -27,6 +27,7 @@ void rk4_push(struct model *model){
     for(size_t i=0; i < num_atoms; i++){
         vector_copy_to(&orig_pos[i], &atoms[i]->position);
         vector_copy_to(&k1[i], &atoms[i]->force);
+        vdiv_by(&k1[i], atoms[i]->mass);
 
         vector_copy_to(&atoms[i]->position, &k1[i]);
         vmul_by(&atoms[i]->position, dt/2);
@@ -37,6 +38,7 @@ void rk4_push(struct model *model){
     #pragma omp parallel for shared(atoms, orig_pos, k1, k2, k3, k4)
     for(size_t i=0; i < num_atoms; i++){
         vector_copy_to(&k2[i], &atoms[i]->force);
+        vdiv_by(&k2[i], atoms[i]->mass);
 
         vector_copy_to(&atoms[i]->position, &k2[i]);
         vmul_by(&atoms[i]->position, dt/2);
@@ -47,6 +49,7 @@ void rk4_push(struct model *model){
     #pragma omp parallel for shared(atoms, orig_pos, k1, k2, k3, k4)
     for(size_t i=0; i < num_atoms; i++){
         vector_copy_to(&k3[i], &atoms[i]->force);
+        vdiv_by(&k3[i], atoms[i]->mass);
 
         vector_copy_to(&atoms[i]->position, &k3[i]);
         vmul_by(&atoms[i]->position, dt);
@@ -57,6 +60,7 @@ void rk4_push(struct model *model){
     #pragma omp parallel for shared(atoms, orig_pos, k1, k2, k3, k4)
     for(size_t i=0; i < num_atoms; i++){
         vector_copy_to(&k4[i], &atoms[i]->force);
+        vdiv_by(&k4[i], atoms[i]->mass);
 
         vmul_by(&k1[i], 1*dt/6);
         vmul_by(&k2[i], 2*dt/6);
