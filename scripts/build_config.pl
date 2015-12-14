994d0500653445cdef1f23eb731571098f832a54
Stefans Mezulis <stefans.mezulis08@imperial.ac.uk>
2015-12-14 12:44:27 +0000

    Add various options for setting poing params
    And a verbose switch

--- scripts/build_config.pl
+++ scripts/build_config.pl
@@ -36,6 +36,26 @@ Psipred SS2 file containing a secondary structure prediction.
 
 Be a little more verbose about what the script is doing.
 
+=item B<--synth-time> I<T>
+
+Wait I<T> time units between each atom synthesis Default: 500
+
+=item B<--until> I<T>
+
+Run for I<T> time units. Default: synth-time x (length + 5)
+
+=item B<--no-sterics>
+
+Do not steric forces. Default: Use sterics.
+
+=item B<--no-shield-drag>
+
+Use simplified drag force. Default: Do not simplify.
+
+=item B<--no-water>
+
+Do not bombard model. Default: Do bombardment.
+
 =item B<-h>, B<--help>
 
 Display this help text.
@@ -54,29 +74,37 @@ GetOptions(\%options,
     'template|t=s@',
     'verbose|v',
     'ss|s=s',
+    'synth-time=f',
+    'until=f',
+    'no-shield-drag',
+    'no-water',
+    'no-sterics',
 ) or pod2usage(2);
 pod2usage(-verbose => 2, -noperldoc => 1, -exitval => 1) if $options{help};
 
 my $query_file = shift || pod2usage('No sequence provided.');
 
-print STDERR "Parsing query\n" if $options{verbose};
 my $query = Bio::Protein::Poing2::Query->new(
     sequence => $query_file,
     ss       => $options{ss},
 );
 my @templates = map {
     my ($aln, $model) = split q{=};
-    print STDERR "Parsing $model\n" if $options{verbose};
     Bio::Protein::Poing2::Template->new(
         alignment => $aln,
         model     => $model,
         query     => $query,
 )} @{$options{template}};
 
-print STDERR "Combining templates and query\n" if $options{verbose};
 my $poing2 = Bio::Protein::Poing2->new(
     query     => $query,
     templates => \@templates,
+    verbose   => $options{verbose},
+    $options{'synth-time'}  ? (synth_time  => $options{'synth-time'}) : (),
+    $options{'until'}       ? (until       => $options{'until'}     ) : (),
+    $options{'no-water'}    ? (use_water   => 0                     ) : (),
+    $options{'no-sterics'}  ? (use_sterics => 0                     ) : (),
+    $options{'shield_drag'} ? (shield_drag => 0                     ) : (),
     spring_filters => [
         Bio::Protein::Poing2::Filter::Pair::SeqSep->new(min_sep => 3),
     ],
--- scripts/lib/Bio/Protein/Poing2.pm
+++ scripts/lib/Bio/Protein/Poing2.pm
@@ -24,6 +24,60 @@ Arrayref of template objects.
 
 has templates => (is => 'ro', isa => 'ArrayRef[Bio::Protein::Poing2::Template]', required => 1);
 
+=item C<until> (Default: C<synth_time * (length + 5)>
+
+Run poing2 until the time reaches this value. The default is based on the
+synthesis time and the length of the protein.
+
+=cut
+
+has until => (is => 'ro', isa => 'Num', lazy => 1, builder => '_build_until');
+
+sub _build_until {
+    my ($self) = @_;
+    return $self->synth_time * ($self->query->length + 5);
+}
+
+=item C<synth_time> (Default: 500)
+
+Time between each atom being synthesised.
+
+=cut
+
+has synth_time => (is => 'ro', isa => 'Num', default => 500);
+
+=item C<drag_coefficient> (Default: -0.5)
+
+Drag coefficient (when not using the shielded drag force).
+
+=cut
+
+has drag_coefficient => (is => 'ro', isa => 'Num', default => -0.5);
+
+=item C<shield_drag> (Default: true)
+
+Use the shielded drag force.
+
+=cut
+
+has shield_drag => (is => 'ro', isa => 'Bool', default => 1);
+
+=item C<use_sterics> (Default: true)
+
+Use the steric force
+
+=cut
+
+has use_sterics => (is => 'ro', isa => 'Bool', default => 1);
+
+=item C<use_water> (Default: true)
+
+Bombard the model with water molecules.
+
+=cut
+
+has use_water => (is => 'ro', isa => 'Bool', default => 1);
+
 =item C<spring_filters>
 
 Filter linear springs with these filters.
@@ -49,6 +103,14 @@ has rama_data => (
     lazy    => 1,
     builder => '_build_rama_data',
 );
+
+=item C<verbose> [optional]
+
+Print progress information to standard error.
+
+=cut
+
+has verbose => (is => 'ro', isa => 'Bool', default => 1);
 
 
 use overload q{""} => \&to_str;
@@ -105,6 +167,11 @@ sub TO_JSON {
     my ($self) = @_;
 
     my %json = (
+        synth_time  => $self->synth_time + 0,
+        until       => $self->until + 0,
+        shield_drag => \$self->shield_drag,
+        use_sterics => \$self->use_sterics,
+        use_water   => \$self->use_water,
         atoms   => [],
         linear  => [],
         angle   => [],
@@ -113,11 +180,13 @@ sub TO_JSON {
     );
 
     #Serialise the sequence into a flat string
+    print STDERR "Building query sequence\n" if $self->verbose;
     my @seq = map {$self->query->residues->{$_}->oneletter}
         sort {$a <=> $b} keys %{$self->query->residues};
     $json{sequence} = join q{}, @seq;
 
     #Build the atom representation
+    print STDERR "Building atom representation\n" if $self->verbose;
     for my $res_i(sort {$a <=> $b} keys %{$self->query->backbone}){
         my $res = $self->query->backbone->{$res_i};
         push @{$json{atoms}}, $_ for @{$res->atoms};
@@ -125,23 +194,32 @@ sub TO_JSON {
 
     #Build linear springs
     #Start with backbone springs
+    print STDERR "Building backbone springs\n" if $self->verbose;
     push @{$json{linear}}, $_ for @{$self->query->backbone_springs};
     #Then add springs from templates
     for my $template(@{$self->templates}){
+        print STDERR "Building springs for ", $template->model, "\n"
+            if $self->verbose;
+
         my $pairs = $template->pairs;
         $pairs = $_->filter($pairs) for @{$self->spring_filters};
         push @{$json{linear}}, $_ for @{$pairs};
     }
 
     #Add angles from query
+    print STDERR "Building bond angles\n" if $self->verbose;
     push @{$json{angle}}, $_ for @{$self->query->angles};
 
     #Add torsion angles from each template
     for my $template(@{$self->templates}){
+        print STDERR "Building torsions for ", $template->model, "\n"
+            if $self->verbose;
+
         push @{$json{torsion}}, $_ for @{$template->fourmers};
     }
 
     #Ramachandran data and constraints
+    print STDERR "Building Ramachandran constraints\n" if $self->verbose;
     $json{ramachandran}->{constraints} = $self->query->ramachandran;
     $json{ramachandran}->{data} = $self->rama_data;
 
